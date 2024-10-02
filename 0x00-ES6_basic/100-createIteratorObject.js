a63fccd34252be869184c4fb385ff7c95035fdc2
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-02 15:53:14 +0100

    Format code solution for task 13 and 14

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,10 @@
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
+  const employees = Object.values(report.allEmployees).flat();
+  return {
+    * [Symbol.iterator]() {
+      for (const employee of employees) {
         yield employee;
       }
-    }
-  }());
+    },
+  };
 }
