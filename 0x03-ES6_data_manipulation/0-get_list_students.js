87ca65afc3e8531cab3cb4d6b9d8bfd9227f4f8c
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-08 16:59:30 +0100

    Add documentation to task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves a list of students.
+ * @author Oluwagbayilo Odetunde 
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
     return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
