f3f15c629e0bd42496d644ddb7e1e177c577a344
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-08 17:35:40 +0100

    Format code in task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,3 @@
-
-/**
- * Updates student grades based on the city they are located in using
- * Object.assign for immutability.
- *
- * @param {Array} studentsList - List of student objects.
- * @param {string} city - City name to filter students by.
- * @param {Array} newGrades - List of grade objects with studentId and grade.
- * @returns {Array} - Updated list of student objects with grades.
- * @throws {TypeError} - If studentsList is not an array, newGrades is not an
- * array, or city is not a string.
- */
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
   if (!Array.isArray(studentsList)) {
     throw new TypeError('studentsList must be an array');
