39d3252cc62c60417a7fea48bb11791200e5e960
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-08 17:32:25 +0100

    Add solution to task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,21 @@
-export function updateStudentGradeByCity(
-    students = [],
-    city = "",
-    grades = []
-) {
-    const defaultGrade = { grade: "N/A" };
-    if (!Array.isArray(students)) {
-        return [];
-    }
 
-    return students
-        .filter((student) => student.location === city)
-        .map((student) => ({
-            id: student.id,
-            firstName: student.firstName,
-            location: student.location,
-            grade: (
-                grades.filter((grade) => grade.studentId === student.id).pop() ||
-                defaultGrade
-            ).grade,
-        }));
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  if (!Array.isArray(studentsList)) {
+    throw new TypeError('studentsList must be an array');
+  }
+
+  if (!Array.isArray(newGrades)) {
+    throw new TypeError('newGrades must be an array');
+  }
+
+  if (typeof city !== 'string') {
+    throw new TypeError('city must be a string');
+  }
+
+  return studentsList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return Object.assign(student, { grade: gradeObj ? gradeObj.grade : 'N/A' });
+    });
 }
