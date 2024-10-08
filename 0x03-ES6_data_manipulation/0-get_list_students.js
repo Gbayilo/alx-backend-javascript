7abe72ab6945272e80852318f6388a9e9d1729fb
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-08 17:55:50 +0100

    Add solution to task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,27 @@
-/**
- * Retrieves a list of students.
- * @author Oluwagbayilo Odetunde 
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
-export default function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-}
+export function getListStudents() {
+    const students = [];
+  
+    const student1 = {
+      id: 1,
+      firstName: "Guillaume",
+      location: "San Francisco",
+    };
+    students.push(student1);
+  
+    const student2 = {
+      id: 2,
+      firstName: "James",
+      location: "Columbia",
+    };
+    students.push(student2);
+  
+    const student3 = {
+      id: 5,
+      firstName: "Serena",
+      location: "San Francisco",
+    };
+    students.push(student3);
+  
+    return students;
+  }
+  
