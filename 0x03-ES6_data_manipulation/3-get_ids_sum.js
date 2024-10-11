e5edc8baa55384ba5c1fd927df6180a13775cf85
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-11 14:20:09 +0100

    Format solution 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,10 @@
-export default function getStudentIdsSum(studentsList) {
-    if (!Array.isArray(studentsList)) {
-        throw new TypeError('studentsList must be an array');
-    }
-
-    return studentsList.reduce((accumulator, student) => {
-        if (!Number.isInteger(student.id)) {
-            throw new TypeError('studentId must be an integer');
-        }
-        return accumulator + student.id;
-    }, 0);
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
 }
+return 0;
+}
+  
